4fc2ab55f9903d689c4a62445b0359b921812d3f
iGhostNeo <ighostudaraka@gmail.com>
2021-01-17 22:46:42 +0530

    fixed video detail error

--- commands/play.js
+++ commands/play.js
@@ -42,93 +42,9 @@ module.exports.run = async (client, message, args, queue, searcher) => {
 
     async function videoHandler(songInfo, message, vc, playlist = false){
         const serverQueue = queue.get(message.guild.id);
-        const song = {
-            title: songInfo.videoDetails.title,
-            url: songInfo.videoDetails.video_url,
-            vLength: songInfo.videoDetails.lengthSeconds,
-            thumbnail: songInfo.videoDetails.thumbnails[0].url
-        }
 
-
-        if(!serverQueue){
-            const queueConstructor = {
-                txtChannel: message.channel,
-                vChannel: vc,
-                connection: null,
-                songs: [],
-                volume: 10,
-                playing: true,
-                loopone: false,
-                loopall: false,
-                skipVotes: []
-            };
-            queue.set(message.guild.id, queueConstructor);
-
-            queueConstructor.songs.push(song);
-
-            try{
-                let connection = await queueConstructor.vChannel.join();
-                queueConstructor.connection = connection;
-                message.guild.me.voice.setSelfDeaf(true);
-                play(message.guild, queueConstructor.songs[0], songInfo, message);
-            }catch (err){
-                console.error(err);
-                queue.delete(message.guild.id);
-                return message.channel.send(`Unable to join the voice chat ${err}`)
-            }
-        }else{
-            serverQueue.songs.push(song);
-            if(playlist) return undefined
-
-
-            let dur = `${parseInt(song.vLength / 60)}:${song.vLength - 60 * parseInt(song.vLength / 60)}`
-            let msg = new Discord.MessageEmbed()
-                .setTitle("Song Added")
-                .addField(song.title, "-----------")
-                .addField("Song duration: ", dur)
-                .addField("Song Place", serverQueue.songs.lastIndexOf(song) + 1)
-                .setThumbnail(song.thumbnail)
-                .setColor("PURPLE")
-            return message.channel.send(msg);
-        }
-    }
-    function play(guild, song, songInfo, message){
-        const serverQueue = queue.get(guild.id);
-        if(!song){
-            serverQueue.vChannel.leave();
-            queue.delete(guild.id);
-            return;
-        }
-        const dispatcher = serverQueue.connection
-            .play(ytdl(song.url))
-            .on('finish', () =>{
-                if(serverQueue.loopone){  
-                    play(guild, serverQueue.songs[0]);
-                }
-                else if(serverQueue.loopall){
-                    serverQueue.songs.push(serverQueue.songs[0])
-                    serverQueue.songs.shift()
-                }else{
-                    serverQueue.songs.shift()
-                }
-                play(guild, serverQueue.songs[0]);
-            })
-
-        const userTag = message.author.tag;
-        const userAvatar = message.author.avatarURL();
         const videoTitle = songInfo.videoDetails.title;
         const videoLength = songInfo.videoDetails.lengthSeconds;
-
-        const videoDescription = songInfo.videoDetails.description;
-        var videoDescriptionSplit = '';
-        if (videoDescription) {
-            videoDescriptionSplit = videoDescription.split('\n');
-        }
-
-        const videoId = songInfo.videoDetails.videoId;
-        const videoArtist = songInfo.videoDetails.media.artist;
-        const videoAlbum = songInfo.videoDetails.media.album;
-        const videoWriters = songInfo.videoDetails.media.writers;
 
         var hours = Math.floor(videoLength / 3600);
         var minutes = Math.floor((videoLength - hours * 3600) / 60);
@@ -154,19 +70,97 @@ module.exports.run = async (client, message, args, queue, searcher) => {
             var set_title = `**${videoTitle} - ${hours}:${minutesSet}:${secondsSet}**`;
         }
 
+        const videoId = songInfo.videoDetails.videoId;
+
+        const videoDescription = songInfo.videoDetails.description;
+        var videoDescriptionSplit = '';
+        if (videoDescription) {
+            videoDescriptionSplit = videoDescription.split('\n');
+        }
+
+        const song = {
+            title: songInfo.videoDetails.title,
+            url: songInfo.videoDetails.video_url,
+            vLength: songInfo.videoDetails.lengthSeconds,
+            thumbnail: songInfo.videoDetails.thumbnails[0].url,
+            embedTitle: set_title,
+            vId: videoId,
+            vDes: `${videoDescriptionSplit[0]}\n${videoDescriptionSplit[1]}`
+        }
+
+
+        if(!serverQueue){
+            const queueConstructor = {
+                txtChannel: message.channel,
+                vChannel: vc,
+                connection: null,
+                songs: [],
+                volume: 10,
+                playing: true,
+                loopone: false,
+                loopall: false,
+                skipVotes: []
+            };
+            queue.set(message.guild.id, queueConstructor);
+
+            queueConstructor.songs.push(song);
+
+            try{
+                let connection = await queueConstructor.vChannel.join();
+                queueConstructor.connection = connection;
+                message.guild.me.voice.setSelfDeaf(true);
+                play(message.guild, queueConstructor.songs[0]);
+            }catch (err){
+                console.error(err);
+                queue.delete(message.guild.id);
+                return message.channel.send(`Unable to join the voice chat ${err}`)
+            }
+        }else{
+            serverQueue.songs.push(song);
+            if(playlist) return undefined
+
+
+            let dur = `${parseInt(song.vLength / 60)}:${song.vLength - 60 * parseInt(song.vLength / 60)}`
+            let msg = new Discord.MessageEmbed()
+                .setTitle("Song Added")
+                .addField(song.title, "-----------")
+                .addField("Song duration: ", dur)
+                .addField("Song Place", serverQueue.songs.lastIndexOf(song) + 1)
+                .setThumbnail(song.thumbnail)
+                .setColor("PURPLE")
+            return message.channel.send(msg);
+        }
+    }
+    function play(guild, song){
+        const serverQueue = queue.get(guild.id);
+        if(!song){
+            serverQueue.vChannel.leave();
+            queue.delete(guild.id);
+            return;
+        }
+        const dispatcher = serverQueue.connection
+            .play(ytdl(song.url))
+            .on('finish', () =>{
+                if(serverQueue.loopone){  
+                    play(guild, serverQueue.songs[0]);
+                }
+                else if(serverQueue.loopall){
+                    serverQueue.songs.push(serverQueue.songs[0])
+                    serverQueue.songs.shift()
+                }else{
+                    serverQueue.songs.shift()
+                }
+                play(guild, serverQueue.songs[0]);
+            })
+
+
         const nowPlayingEmbed = new Discord.MessageEmbed()
-                .setAuthor('YouTube', 'https://i.imgur.com/dFd53fY.png', 'https://www.youtube.com/')
-                .setTitle(set_title)
-                .setDescription(`${videoDescriptionSplit[0]}\n${videoDescriptionSplit[1]}`)
-                .setURL(`https://www.youtube.com/watch?v=${videoId}`)
+                .setAuthor('Now Playing...', 'https://i.imgur.com/dFd53fY.png')
+                .setTitle(song.embedTitle)
+                .setDescription(song.vDes)
+                .setURL(`https://www.youtube.com/watch?v=${song.vId}`)
                 .setColor('#ff0000')
-                .addFields(
-                    { name: 'Artist', value: videoArtist, inline: true },
-                    { name: 'Album', value: videoAlbum, inline: true },
-                    { name: 'Writers', value: videoWriters, inline: true }
-                )
-                .setThumbnail(`https://img.youtube.com/vi/${videoId}/0.jpg`)
-                .setFooter(userTag, userAvatar)
+                .setThumbnail(`https://img.youtube.com/vi/${song.vId}/0.jpg`)
                 .setTimestamp();
 
         // let dur = `${parseInt(serverQueue.songs[0].vLength / 60)}:${serverQueue.songs[0].vLength - 60 * parseInt(serverQueue.songs[0].vLength / 60)}`
